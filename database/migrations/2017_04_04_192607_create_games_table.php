7613edc56ce843f5761cb130c9c458b2130d7033
Max Prokopenko <Max Prokopenko>
2017-04-04 22:36:19 +0300

    Migrations for teachers, games and spots added

---
+++ database/migrations/2017_04_04_192607_create_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->json('tasks');
+            $table->integer('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
---
+++ database/migrations/2017_04_04_193335_create_spots_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('desc');
+            $table->json('coords');
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spots');
+    }
+}
