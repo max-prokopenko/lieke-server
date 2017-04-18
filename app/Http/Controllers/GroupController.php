6f82a111e9d891710502bb696ec97adb967846b3
Max Prokopenko <max.prokopenko@hotmail.com>
2017-04-18 07:15:53 +0300

    First release

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -19,7 +19,7 @@ class GroupController extends Controller
     public function index()
     {
         //Remember to change to auth
-        
+        //   
         //$games = Game::all()->where('teacher_id', Auth::guard('teacher')->user()->id);
         $groups = Group::all()->where('teacher_id', Auth::guard('teacher')->user()->id);
                 //$groups = Group::all()->where('teacher_id', Auth::user()->id);
